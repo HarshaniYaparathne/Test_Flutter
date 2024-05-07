cab7cbe8023c32e5094f7aefa435cf39f35ee1e3
HarshaniYaparathne <ymhdyaparathne@std.appsc.sab.ac.lk>
2024-05-07 22:14:25 +0530

    modify HomePage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -8,22 +8,37 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hi everyone !'),
+        backgroundColor: Colors.blue, // Set the app bar color to blue
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Welcome to HomePage!',
-              style: TextStyle(fontSize: 24.0),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Add your action here
-              },
-              child: const Text('Click Me'),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.blue[200]!,
+              Colors.blue[400]!,
+              Colors.blue[600]!,
+              Colors.blue[800]!,
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Welcome to HomePage!',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your action here
+                },
+                child: const Text('Click Me'),
+              ),
+            ],
+          ),
         ),
       ),
     );
